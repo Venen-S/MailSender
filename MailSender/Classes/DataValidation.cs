7f9ca5fea4a4b35135e4bba5e6526512ae35f95f
Николай <semlya.kolyan@mail.ru>
2020-01-10 17:53:28 +0300

    Добавлена валидация данных

---
+++ MailSender/Classes/DataValidation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace MailSender.Classes
+{
+    /// <summary>
+    /// Класс для проверки что ID получателя больше 0
+    /// </summary>
+    public class DataValidation:ValidationRule
+    {
+        public override ValidationResult Validate(object value,
+            System.Globalization.CultureInfo cultureInfo)
+        {
+            int IntValue = 0;
+            ValidationResult result = null;
+            try
+            {
+                IntValue = Convert.ToInt32(value);
+            }
+            catch (Exception)
+            {
+                return new ValidationResult(false, "Введите число");
+            }
+            if (IntValue < 0) return new ValidationResult(false, "Введите число больше 0");
+            return new ValidationResult(true, null);
+        }
+    }
+}
---
+++ MailSender/Classes/Recipients.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailSender.Classes
+{
+    /// <summary>
+    /// Пользовательская валидация данных в поле Email
+    /// </summary>
+    public partial class Recipients : INotifyPropertyChanging, INotifyPropertyChanged, IDataErrorInfo
+    {
+        public string Error
+        {
+            get
+            {
+                return "";
+            }
+        }
+        public string this[string propertyName]
+        {
+            get
+            {
+                if (propertyName == "Address")
+                {
+                    if (_Address != null && _Address.Length < 4) return "Необходимо указать правильный e-mail!";
+                }
+                return "";
+            }
+        }
+    }
+}
